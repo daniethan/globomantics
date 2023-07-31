5c585934660d21cc9ae0289a49aeb6dec81c840e
nsozi_daniel <nsozidaniel@brackisoft.com>
2023-07-31 11:48:30 +0300

    Initial commit

---
+++ src/main-page/index.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './main-page.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './header';
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
